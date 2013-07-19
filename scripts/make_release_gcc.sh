35da8d3810af8353a2f58f992f5bac4f1dc1d375
Michael Pippig <michael.pippig@mathematik.tu-chemnitz.de>
2013-07-19 07:30:16 +0200

    add scripts for creating PFFT release (PFFT-SVN r3525)

---
+++ scripts/make_release_gcc.sh
@@ -0,0 +1,13 @@
+#!/bin/sh -e
+
+# change version numbers before new release
+OLD_FFTW_VERSION=3.3.2
+NEW_FFTW_VERSION=3.3.3
+
+OLD_PFFT_VERSION=1.0.5-alpha
+NEW_PFFT_VERSION=1.0.6-alpha
+
+sed -i "s/\(FFTW_VERSION=\)$OLD_FFTW_VERSION/\1$NEW_FFTW_VERSION/" *.sh
+sed -i "s/\(PFFT_VERSION=\)$OLD_PFFT_VERSION/\1$NEW_PFFT_VERSION/" *.sh
+
+bash build_release.sh
