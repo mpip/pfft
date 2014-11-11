fbdd154a5e8a57855efde39a22f748df3fe69032
Michael Pippig <michael.pippig@mathematik.tu-chemnitz.de>
2014-11-11 12:50:10 +0100

    do not clear input in stand alone forward FFT test for user manual

--- tests/manual_c2c_3d.c
+++ tests/manual_c2c_3d.c
@@ -42,10 +42,6 @@ int main(int argc, char **argv){
   /* Execute parallel forward FFT */
   pfft_execute(plan);
 
-  /* clear the old input */
-  pfft_clear_input_complex_3d(n, local_ni, local_i_start,
-      in);
-  
   /* free mem and finalize MPI */
   pfft_destroy_plan(plan);
   MPI_Comm_free(&comm_cart_2d);
