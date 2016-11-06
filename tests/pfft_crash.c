5948ad83b6d92fa5e8fb9488d1f16bc601c46c96
Michael Pippig <michael.pippig.tuc@gmail.com>
2016-11-06 21:54:32 +0100

    simplify crash test, bug fixed within FFTW-3.3.5

--- tests/pfft_crash.c
+++ tests/pfft_crash.c
@@ -8,14 +8,11 @@ int main(int argc, char **argv)
   ptrdiff_t alloc_local;
   ptrdiff_t local_ni[2], local_i_start[2];
   ptrdiff_t local_no[2], local_o_start[2];
-  double err;
-  pfft_complex *in, *out;
-  pfft_plan plan_forw=NULL, plan_back=NULL;
   MPI_Comm comm_cart_1d;
 
   /* Set size of FFT and process mesh */
-  n[0] = 200; n[1] = 200;
-  np[0] = 12;
+  n[0] = 3; n[1] = 3;
+  np[0] = 2;
 
   /* Initialize MPI and PFFT */
   MPI_Init(&argc, &argv);
@@ -34,44 +31,37 @@ int main(int argc, char **argv)
   alloc_local = pfft_local_size_dft(2, n, comm_cart_1d, PFFT_TRANSPOSED_NONE,
       local_ni, local_i_start, local_no, local_o_start);
 
+//   int rnk;
+//   MPI_Comm_rank(comm_cart_1d, &rnk);
+//   for(int r=0; r<np[0]; r++){
+//     if(r==rnk){
+//       printf("local_ni = [%td, %td]\n", local_ni[0], local_ni[1]);
+//       printf("local_i_start = [%td, %td]\n", local_i_start[0], local_i_start[1]);
+//       printf("local_no = [%td, %td]\n", local_no[0], local_no[1]);
+//       printf("local_o_start = [%td, %td]\n", local_o_start[0], local_o_start[1]);
+//     }
+//     MPI_Barrier(comm_cart_1d);
+//   }
+
   /* Allocate memory */
-  in  = pfft_alloc_complex(alloc_local);
-  out = pfft_alloc_complex(alloc_local);
+  pfft_complex *in  = pfft_alloc_complex(alloc_local);
+  pfft_complex *out = pfft_alloc_complex(alloc_local);
 
   /* Plan parallel forward FFT */
-  plan_forw = pfft_plan_dft(
+//   const unsigned flags = PFFT_TRANSPOSED_NONE| PFFT_ESTIMATE;
+//   pfft_plan plan1 = pfft_plan_dft(
+//       2, n, in, in, comm_cart_1d, PFFT_FORWARD, flags);
+//   pfft_plan plan2 = pfft_plan_dft(
+//       2, n, in, in, comm_cart_1d, PFFT_FORWARD, flags);
+
+  pfft_plan plan1 = pfft_plan_dft(
+      2, n, in, out, comm_cart_1d, PFFT_FORWARD, PFFT_TRANSPOSED_NONE| PFFT_ESTIMATE| PFFT_PRESERVE_INPUT);
+  pfft_plan plan2 = pfft_plan_dft(
       2, n, in, out, comm_cart_1d, PFFT_FORWARD, PFFT_TRANSPOSED_NONE| PFFT_ESTIMATE| PFFT_PRESERVE_INPUT);
 
-  /* Plan parallel backward FFT */
-  plan_back = pfft_plan_dft(
-      2, n, out, in, comm_cart_1d, PFFT_BACKWARD, PFFT_TRANSPOSED_NONE| PFFT_ESTIMATE| PFFT_PRESERVE_INPUT);
-
-  /* Initialize input with random numbers */
-  pfft_init_input_complex(2, n, local_ni, local_i_start,
-      in);
-
-  /* execute parallel forward FFT */
-  pfft_execute(plan_forw);
-
-  /* clear the old input */
-  pfft_clear_input_complex(2, n, local_ni, local_i_start,
-      in);
-
-  /* execute parallel backward FFT */
-  pfft_execute(plan_back);
-
-  /* Scale data */
-  for(ptrdiff_t l=0; l < local_ni[0] * local_ni[1]; l++)
-    in[l] /= (n[0]*n[1]);
-
-  /* Print error of back transformed data */
-  err = pfft_check_output_complex(2, n, local_ni, local_i_start, in, comm_cart_1d);
-  pfft_printf(comm_cart_1d, "Error after one forward and backward trafo of size n=(%td, %td):\n", n[0], n[1]); 
-  pfft_printf(comm_cart_1d, "maxerror = %6.2e;\n", err);
-
   /* free mem and finalize */
-  pfft_destroy_plan(plan_forw);
-  pfft_destroy_plan(plan_back);
+  pfft_destroy_plan(plan1);
+  pfft_destroy_plan(plan2);
   MPI_Comm_free(&comm_cart_1d);
   pfft_free(in); pfft_free(out);
   MPI_Finalize();
