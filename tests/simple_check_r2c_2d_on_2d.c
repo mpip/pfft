7fe0f47a235a7f9ab9cb06a3a21006569023dddd
Yu Feng <rainwoodman@gmail.com>
2018-01-17 19:25:09 -0800

    fix the 2don2d test case on 1x1.

--- tests/simple_check_r2c_2d_on_2d.c
+++ tests/simple_check_r2c_2d_on_2d.c
@@ -1,6 +1,26 @@
 #include <complex.h>
 #include <pfft.h>
 
+static void printarray(double * a, int n0, int n1)
+{
+  for(ptrdiff_t l=0; l < n0 * n1; l++) {
+    pfft_fprintf(MPI_COMM_WORLD, stderr, "%4.2f ", a[l]);
+    if ((l + 1) % n1 == 0) {
+       pfft_fprintf(MPI_COMM_WORLD, stderr, "\n", a[l]);
+   }
+  }
+}
+static void gatherarray(double * a, double * in, int local_start0,
+        int local_n0, int n0, int local_start1, int local_n1, int n1)
+{
+  memset(a, 0, sizeof(double) * n0 * n1);
+  for(ptrdiff_t l=0; l < local_n0 * local_n1; l++) {
+    int x = l / local_n1, y = l % local_n1;
+    a[(local_start0 + x) * n1 + local_start1 + y] = in[l];
+  }
+  MPI_Allreduce(MPI_IN_PLACE, a, n0 * n1, MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD);
+}
+
 int main(int argc, char **argv)
 {
   int np[2];
@@ -14,9 +34,12 @@ int main(int argc, char **argv)
   MPI_Comm comm_cart_2d;
   
   /* Set size of FFT and process mesh */
-  n[0] = 29; n[1] = 27;
-  np[0] = 2; np[1] = 2;
+  n[0] = 4; n[1] = 4;
+  np[0] = 1; np[1] = 1;
   
+  double *truein = calloc(sizeof(double), n[0] * n[1]);
+  double *trueout = calloc(sizeof(double), n[0] * (n[1] / 2 + 1) * 2);
+
   /* Initialize MPI and PFFT */
   MPI_Init(&argc, &argv);
   pfft_init();
@@ -32,44 +55,70 @@ int main(int argc, char **argv)
   alloc_local = pfft_local_size_dft_r2c(2, n, comm_cart_2d, PFFT_TRANSPOSED_NONE,
       local_ni, local_i_start, local_no, local_o_start);
 
-  pfft_fprintf(MPI_COMM_WORLD, stderr, "%td.\n", alloc_local);
-
   /* Allocate memory */
   in  = pfft_alloc_real(2 * alloc_local);
   out = pfft_alloc_complex(alloc_local);
 
+  pfft_fprintf(MPI_COMM_WORLD, stderr, "planning forward\n");
   /* Plan parallel forward FFT */
   plan_forw = pfft_plan_dft_r2c(2,
-      n, in, out, comm_cart_2d, PFFT_FORWARD, PFFT_TRANSPOSED_NONE| PFFT_MEASURE| PFFT_DESTROY_INPUT);
+      n, in, out, comm_cart_2d, PFFT_FORWARD, PFFT_TRANSPOSED_NONE| PFFT_ESTIMATE| PFFT_DESTROY_INPUT);
   
+  pfft_fprintf(MPI_COMM_WORLD, stderr, "planning backward\n");
   /* Plan parallel backward FFT */
   plan_back = pfft_plan_dft_c2r(2, 
-      n, out, in, comm_cart_2d, PFFT_BACKWARD, PFFT_TRANSPOSED_NONE| PFFT_MEASURE| PFFT_DESTROY_INPUT);
+      n, out, in, comm_cart_2d, PFFT_BACKWARD, PFFT_TRANSPOSED_NONE| PFFT_ESTIMATE| PFFT_DESTROY_INPUT);
 
   /* Initialize input with random numbers */
   pfft_init_input_real(2, n, local_ni, local_i_start,
       in);
 
+  for(ptrdiff_t l=0; l < local_ni[0] * local_ni[1]; l++) {
+    in[l] = l;
+//    if(l == 0)
+//        in[l] = 1;
+  }
+  gatherarray(truein, in, local_i_start[0], local_ni[0], n[0],
+                          local_i_start[1], local_ni[1], n[1]);
+  printarray(truein, n[0], n[1]);
+
+  pfft_fprintf(MPI_COMM_WORLD, stderr, "running forward\n");
   /* execute parallel forward FFT */
   pfft_execute(plan_forw);
 
+  fprintf(stderr, "%td %td %td %td\n", local_o_start[0], local_no[0], local_o_start[1], local_no[1]);
+
+  gatherarray(trueout, out, local_o_start[0], local_no[0], n[0],
+                            2 * local_o_start[1],
+                            2 * local_no[1], 2 *(n[1] / 2 + 1));
+
+  printarray(trueout, n[0], (n[1] / 2 + 1) * 2);
+  //gatherarray(truein, out, local_i_start[0], local_ni[0], n[0],
+  //                        local_i_start[1], local_ni[1], n[1]);
+  //printarray(truein, n[0], n[1]);
+
+  pfft_fprintf(MPI_COMM_WORLD, stderr, "running backward\n");
   /* clear the old input */
   pfft_clear_input_real(2, n, local_ni, local_i_start,
       in);
-  
   /* execute parallel backward FFT */
   pfft_execute(plan_back);
-  
+
   /* Scale data */
   for(ptrdiff_t l=0; l < local_ni[0] * local_ni[1]; l++)
     in[l] /= (n[0]*n[1]);
-  
+
+  gatherarray(truein, in, local_i_start[0], local_ni[0], n[0],
+                          local_i_start[1], local_ni[1], n[1]);
+  printarray(truein, n[0], n[1]);
+
+#if 0
   /* Print error of back transformed data */
   MPI_Barrier(MPI_COMM_WORLD);
   err = pfft_check_output_real(2, n, local_ni, local_i_start, in, comm_cart_2d);
-  pfft_printf(comm_cart_2d, "Error after one forward and backward trafo of size n=(%td, %td, %td):\n", n[0], n[1]); 
+  pfft_printf(comm_cart_2d, "Error after one forward and backward trafo of size n=(%td, %td):\n", n[0], n[1]); 
   pfft_printf(comm_cart_2d, "maxerror = %6.2e;\n", err);
-  
+ #endif 
   /* free mem and finalize */
   pfft_destroy_plan(plan_forw);
   pfft_destroy_plan(plan_back);
